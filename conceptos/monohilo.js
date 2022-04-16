ce121a9c935f31238b463d44eab5df450e61d5cc
Jose Morales <josemorales-1234@hotmail.es>
2022-04-16 19:44:56 -0400

    Archivos monohilo:revisar contantemente el flujo del codigo para evitar errores, un solo error detiene todo el programa

---
+++ conceptos/monohilo.js
@@ -0,0 +1,13 @@
+console.log('Hola mundo')
+
+let i = 0
+setInterval(function(){
+  console.log(i)
+  console.log('Sigo activo')
+  i++
+  if (i==5){
+    var a = 3+z
+  }
+}, 1000)
+
+console.log('Chao mundo')
